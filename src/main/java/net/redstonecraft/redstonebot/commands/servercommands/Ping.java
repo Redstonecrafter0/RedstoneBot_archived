ef6d619f9e79b8dd865528fb93f22ba3a4ffac99
Redstonecrafter0 <54239558+Redstonecrafter0@users.noreply.github.com>
2020-08-07 15:26:24 +0200

    added ping command

---
+++ src/main/java/net/redstonecraft/redstonebot/commands/servercommands/Ping.java
@@ -0,0 +1,25 @@
+package net.redstonecraft.redstonebot.commands.servercommands;
+
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.redstonecraft.redstonebot.Main;
+import net.redstonecraft.redstonebot.interfaces.ServerCommand;
+
+public class Ping implements ServerCommand {
+    @Override
+    public boolean onCommand(TextChannel channel, Member member, Message message, String[] args) {
+        return false;
+    }
+
+    @Override
+    public MessageEmbed.Field help() {
+        return null;
+    }
+
+    @Override
+    public String usage() {
+        return Main.commandPrefix + " ping";
+    }
+}
