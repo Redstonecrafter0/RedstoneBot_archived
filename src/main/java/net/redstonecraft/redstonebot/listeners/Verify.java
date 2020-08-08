b44e7bbf2288f44676bfad178ee9eb88c52d6e36
Redstonecrafter0 <54239558+Redstonecrafter0@users.noreply.github.com>
2020-08-08 12:02:26 +0200

    added welcome cards

--- src/main/java/net/redstonecraft/redstonebot/listeners/Verify.java
+++ src/main/java/net/redstonecraft/redstonebot/listeners/Verify.java
@@ -8,9 +8,16 @@ import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.redstonecraft.redstonebot.Main;
-import org.jetbrains.annotations.NotNull;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Objects;
@@ -86,9 +93,34 @@ public class Verify extends ListenerAdapter {
                 event.getGuild().removeRoleFromMember(event.getMember(), Objects.requireNonNull(event.getGuild().getRoleById((String) Main.config.get("unverifiedRole")))).queue();
                 event.getGuild().addRoleToMember(event.getMember(), Objects.requireNonNull(event.getGuild().getRoleById((String) Main.config.get("verifiedRole")))).queue();
                 Main.sql.update("UPDATE members SET verified = '1' WHERE dcId = '" + event.getMember().getId() + "'");
+                File file = new File("tmp.png");
+                BufferedImage bufferedImage = new BufferedImage(1000, 500, BufferedImage.TYPE_INT_ARGB);
+                Graphics graphics = bufferedImage.createGraphics();
+                graphics.setColor(Color.decode("#FFFFFF"));
+                Font font = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream("font.ttf")).deriveFont(50F);
+                graphics.setFont(font);
+                FontMetrics metrics = graphics.getFontMetrics();
+                graphics.drawString("Willkommen", 500 - (metrics.stringWidth("Willkommen") / 2), 400);
+                graphics.drawString(event.getMember().getUser().getAsTag(), 500 - (metrics.stringWidth(event.getMember().getUser().getAsTag()) / 2), 460);
+                HttpURLConnection connection;
+                if (event.getMember().getUser().getAvatarUrl() != null) {
+                    connection = (HttpURLConnection) new URL(Objects.requireNonNull(event.getMember().getUser().getAvatarUrl())).openConnection();
+                } else {
+                    connection = (HttpURLConnection) new URL(Objects.requireNonNull(event.getMember().getUser().getDefaultAvatarUrl())).openConnection();
+                }
+                connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31");
+                graphics.setClip(new Ellipse2D.Float(350, 30, 300, 300));
+                graphics.drawImage(ImageIO.read(connection.getInputStream()), 350, 30, 300, 300, null);
+                if (!file.exists()) {
+                    file.createNewFile();
+                }
+                ImageIO.write(bufferedImage, "png", file);
+                Objects.requireNonNull(event.getGuild().getTextChannelById((String) Main.config.get("welcome"))).sendFile(file).append("Willkommen auf dem Server ").append(event.getMember().getUser().getName()).queue();
             }
         } catch (SQLException e) {
             Main.getLogger().warning(e.getMessage());
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
         }
         event.getMessage().delete().queue();
     }
