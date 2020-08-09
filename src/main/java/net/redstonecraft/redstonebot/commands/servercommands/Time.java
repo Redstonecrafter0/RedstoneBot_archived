b038117359c0b6ddf6a91f94c2a2fcb83928f8f6
Redstonecrafter0 <54239558+Redstonecrafter0@users.noreply.github.com>
2020-08-09 21:26:01 +0200

    added time command

---
+++ src/main/java/net/redstonecraft/redstonebot/commands/servercommands/Time.java
@@ -0,0 +1,34 @@
+package net.redstonecraft.redstonebot.commands.servercommands;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.redstonecraft.redstonebot.Main;
+import net.redstonecraft.redstonebot.interfaces.ServerCommand;
+
+import java.awt.*;
+import java.text.SimpleDateFormat;
+
+public class Time implements ServerCommand {
+    @Override
+    public boolean onCommand(TextChannel channel, Member member, Message message, String[] args) {
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setTitle(Main.prefix);
+        eb.setColor(Color.decode("#3498DB"));
+        eb.setDescription("Es ist " + new SimpleDateFormat("HH:mm:ss").format(System.currentTimeMillis()) + ".");
+        channel.sendMessage(eb.build()).queue();
+        return true;
+    }
+
+    @Override
+    public MessageEmbed.Field help() {
+        return new MessageEmbed.Field(Main.commandPrefix + " time", "Zeigt die aktuelle Server zeit.", false);
+    }
+
+    @Override
+    public String usage() {
+        return Main.commandPrefix + " time";
+    }
+}
