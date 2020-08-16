f2fdb15ee6cf50ab7edcdc74325860a89593eefc
Redstonecrafter0 <54239558+Redstonecrafter0@users.noreply.github.com>
2020-08-16 11:20:37 +0200

    added patch system

---
+++ src/main/java/net/redstonecraft/redstonebot/MinecraftPatches.java
@@ -0,0 +1,48 @@
+package net.redstonecraft.redstonebot;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.redstonecraft.utils.Request;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.awt.*;
+import java.util.Objects;
+
+public class MinecraftPatches {
+
+    public void run() {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(180000);
+                    Request request = new Request("https://launchermeta.mojang.com/mc/game/version_manifest.json");
+                    request.connect();
+                    JSONObject root = (JSONObject) new JSONParser().parse(request.getResponse());
+                    JSONArray versions = (JSONArray) root.get("versions");
+                    JSONObject version = (JSONObject) versions.get(0);
+                    String id = (String) version.get("id");
+                    if (!id.equals((String) Main.config.get("latestMcVersion"))) {
+                        String type = (String) version.get("type");
+                        EmbedBuilder eb = new EmbedBuilder();
+                        eb.setTitle(Main.prefix);
+                        eb.setColor(Color.decode("#3498DB"));
+                        if (type.equals("release")) {
+                            eb.setDescription("Es gibt eine neue Version im Minecraftlauncher.");
+                        } else if (type.equals("snapshot")) {
+                            eb.setDescription("Es gibt einen neuen Snapshot im Minecraftlauncher.");
+                        }
+                        eb.addField(type.toUpperCase(), id, false);
+                        Main.config.remove("latestMcVersion");
+                        Main.config.put("latestMcVersion", id);
+                        Main.saveConfig();
+                        Objects.requireNonNull(Objects.requireNonNull(Discord.INSTANCE.getManager().getGuildById((String) Main.config.get("guild"))).getTextChannelById((String) Main.config.get("patchchannel"))).sendMessage(eb.build()).queue();
+                    }
+                } catch (InterruptedException | ParseException ignored) {
+                }
+            }
+        }).start();
+    }
+
+}
---
+++ src/main/java/net/redstonecraft/redstonebot/commands/servercommands/SetPatchChannel.java
@@ -0,0 +1,47 @@
+package net.redstonecraft.redstonebot.commands.servercommands;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.redstonecraft.redstonebot.Main;
+import net.redstonecraft.redstonebot.interfaces.ServerCommand;
+
+import java.awt.*;
+import java.util.Objects;
+
+public class SetPatchChannel implements ServerCommand {
+    @Override
+    public boolean onCommand(TextChannel channel, Member member, Message message, String[] args) {
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setTitle(Main.prefix);
+        if (member.hasPermission(Permission.ADMINISTRATOR)) {
+            if (channel.getGuild().getTextChannelById(args[1]) != null) {
+                Main.config.remove("patchchannel");
+                Main.config.put("patchchannel", args[1]);
+                eb.setDescription("Der neue Patch Channel ist " + Objects.requireNonNull(channel.getGuild().getTextChannelById(args[1])).getAsMention());
+                eb.setColor(Color.decode("#00FF00"));
+            } else {
+                eb.setDescription("Es gab einen Fehler.");
+                eb.setColor(Color.decode("#FF0000"));
+            }
+        } else {
+            eb.setColor(Color.decode("#FF0000"));
+            eb.setDescription("Dir fehlt die Berechtigung Administrator.");
+        }
+        channel.sendMessage(eb.build()).queue();
+        return true;
+    }
+
+    @Override
+    public MessageEmbed.Field help() {
+        return new MessageEmbed.Field(Main.commandPrefix + " setpatchchannel", "Setzt den Patchchannel", false);
+    }
+
+    @Override
+    public String usage() {
+        return Main.commandPrefix + " setpatchchannel [id]";
+    }
+}
